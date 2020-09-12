22279de00dcadc895ff4ec65bdc853c98dfeb5ac
13events <jdmartinez90@icloud.com>
2020-09-11 19:52:11 -0700

    Refactored Code
    -Refactored code in article_controller
    -Created partials for article creation/edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,7 @@
 class ArticlesController < ApplicationController
-
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
   #Shows the selected article
   def show
-    @article = Article.find(params[:id])
   end
 
   #display all articles
@@ -17,13 +16,13 @@ class ArticlesController < ApplicationController
   #Create new article
   def create
     #create new article and require 'article' top key, permit 'title', 'description' keys
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     #save article to db
     @article.save
 
     #check for erros saving to db, otherwise redirect
     if @article.errors.count > 0
-      render 'new'
+      render "new"
     else
       flash[:notice] = "Article Created Successfully"
       redirect_to articles_path
@@ -32,24 +31,30 @@ class ArticlesController < ApplicationController
 
   #find article to edit, so we can display its information in the form
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = "Article Edited Successfully!"
       redirect_to articles_path
-    else 
-      render 'edit'
+    else
+      render "edit"
     end
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Article \"#{@article.title}\" wad deleted!"
     redirect_to articles_path
   end
 
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
