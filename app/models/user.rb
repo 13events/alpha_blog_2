4addd22d46ec14037439d17ebdaabbb71e8b356f
13events <jdmartinez90@icloud.com>
2020-09-24 17:03:37 -0700

    Added User Validations
    * Added username validation
    * Added email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
-  
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, 
+                                      length: { minimum: 3, maximum: 30 }
+
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, 
+                                      length: { maximum: 100 }, format: {with: URI::MailTo::EMAIL_REGEXP }
+
+
 end
