0ab4e4ba4e86dad119324f0e102e9e7776cff4d0
13events <jdmartinez90@icloud.com>
2020-09-26 16:10:08 -0700

    Added before_save to User model
     * Change user email to downcase before sacing to DB

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  #change email to lowecase before saving to DB
+  before_save {self.email = email.downcase}
+
   validates :username, presence: true, uniqueness: { case_sensitive: false }, 
                                       length: { minimum: 3, maximum: 30 }
 
