1f85f6a51549bce70405b2cf1a76170f48cdb245
13events <jdmartinez90@icloud.com>
2020-10-01 17:20:49 -0700

    Added User Profile Page
    * Added Show method to users model
    * added user show view
    * Created helper method to grab gravatar profile picture
    * Created articles partial
    * article/index renders articles partial
    *

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
   def create
     #create a new user with white listed params
     @user = User.new(user_params)
