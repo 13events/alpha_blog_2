0eb5d3a0b1598c0cafaeb2a29d555eca51933270
13events <jdmartinez90@icloud.com>
2021-02-25 17:25:45 -0800

    Added article_categories table and associations * Created article_categories_test * article_categories migration and table added * ArticleCategory model created * Added associations to Article and User

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ApplicationRecord
+  has_many :article_categories
+  has_many :article, through: :article_categories
   validates :name, presence: true, uniqueness: true, length: {maximum: 20, minimum: 4}
-
 end
